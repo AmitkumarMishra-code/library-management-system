ed43ac10694ad9120c53617a624427dec9275698
Amitkumar Mishra <mr.mash@gmail.com>
2021-08-03 16:09:34 +0530

    fixed port

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.all(/.*/, (req, res) => {
 
 
 
-const PORT = 3300
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
 })
